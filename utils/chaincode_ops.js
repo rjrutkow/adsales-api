a257f1266dde96ea07ed66f3bef3b629005aa41c
gsc <gsc@gsc-RY-macbook.local>
2017-03-08 14:00:50 -0500

    add additional helpers

--- utils/chaincode_ops.js
+++ utils/chaincode_ops.js
@@ -50,7 +50,7 @@ CPChaincode.prototype.releaseInventory = function (uid, inputArgs, cb) {
 
     invoke(this.chain, uid, releaseInventory, function (err, result) {
         if (err) {
-            console.error(TAG, 'failed release Inventory:', err);
+            console.error(TAG, 'failed releaseInventory:', err);
             return cb(err);
         }
 
@@ -62,48 +62,142 @@ CPChaincode.prototype.releaseInventory = function (uid, inputArgs, cb) {
 CPChaincode.prototype.queryPlaceOrders = function (enrollID, inputArgs, cb) {
     console.log(TAG, 'queryPlaceOrders - chaincode_ops:', enrollID);
 
-    var queryPlaceOrdersReq = {
+    var queryPlaceOrders = {
         chaincodeID: this.chaincodeID,
         fcn: 'queryPlaceOrders',
         args: inputArgs
     };
 
-    query(this.chain, enrollID, queryPlaceOrdersReq, function (err, qResponse) {
+    query(this.chain, enrollID, queryPlaceOrders, function (err, qResponse) {
         if (err) {
-            console.error(TAG, 'failed to get queryPlaceOrdersReq:', err);
+            console.error(TAG, 'failed to get queryPlaceOrders:', err);
             return cb(err);
         }
 
-        console.log(TAG, 'retrieved queryPlaceOrdersReq information:', qResponse.toString());
+        console.log(TAG, 'retrieved queryPlaceOrders information:', qResponse.toString());
+        cb(null, qResponse.toString());
+    });
+};
+
+CPChaincode.prototype.placeOrders = function (uid, inputArgs, cb) {
+    console.log(TAG, '- placeOrders uid: ', uid);
+    console.log(TAG, '- placeOrders input args: ', JSON.stringify(inputArgs));
+    var placeOrders = {
+        chaincodeID: this.chaincodeID,
+        fcn: 'placeOrders',
+        args: inputArgs
+    };
+
+    invoke(this.chain, uid, placeOrders, function (err, result) {
+        if (err) {
+            console.error(TAG, 'failed placeOrders:', err);
+            return cb(err);
+        }
+
+        console.log(TAG, 'placeOrders successfully:', result.toString());
+        cb(null, result);
+    });
+}
+
+CPChaincode.prototype.queryAdspotsToMap = function (enrollID, inputArgs, cb) {
+    console.log(TAG, 'queryAdspotsToMap - chaincode_ops:', enrollID);
+
+    var queryAdspotsToMap = {
+        chaincodeID: this.chaincodeID,
+        fcn: 'queryAdspotsToMap',
+        args: inputArgs
+    };
+
+    query(this.chain, enrollID, queryAdspotsToMap, function (err, qResponse) {
+        if (err) {
+            console.error(TAG, 'failed to get queryAdspotsToMap:', err);
+            return cb(err);
+        }
+
+        console.log(TAG, 'retrieved queryAdspotsToMap information:', qResponse.toString());
+        cb(null, qResponse.toString());
+    });
+};
+
+CPChaincode.prototype.mapAdspots = function (uid, inputArgs, cb) {
+    console.log(TAG, '- mapAdspots uid: ', uid);
+    console.log(TAG, '- mapAdspots input args: ', JSON.stringify(inputArgs));
+    var mapAdspots = {
+        chaincodeID: this.chaincodeID,
+        fcn: 'mapAdspots',
+        args: inputArgs
+    };
+
+    invoke(this.chain, uid, mapAdspots, function (err, result) {
+        if (err) {
+            console.error(TAG, 'failed mapAdspots:', err);
+            return cb(err);
+        }
+
+        console.log(TAG, 'mapAdspots successfully:', result.toString());
+        cb(null, result);
+    });
+}
+
+CPChaincode.prototype.queryAsRun = function (enrollID, inputArgs, cb) {
+    console.log(TAG, 'queryAsRun - chaincode_ops:', enrollID);
+
+    var queryAsRun = {
+        chaincodeID: this.chaincodeID,
+        fcn: 'queryAsRun',
+        args: inputArgs
+    };
+
+    query(this.chain, enrollID, queryAsRun, function (err, qResponse) {
+        if (err) {
+            console.error(TAG, 'failed to get queryAsRun:', err);
+            return cb(err);
+        }
+
+        console.log(TAG, 'retrieved queryAsRun information:', qResponse.toString());
         cb(null, qResponse.toString());
     });
 };
 
 
-/**
- * Create an account on the commercial paper trading network.  The given enrollID will also be taken as the name for the
- * commercial paper trading account.
- * @param enrollID The enrollID for the user submitting the transaction.
- * @param cb A callback function of the form: function(error)
- */
-CPChaincode.prototype.createCompany = function (enrollID, cb) {
-    console.log(TAG, 'Creating a company for:', enrollID);
-
-    // Accounts will be named after the enrolled users
-    var createRequest = {
+CPChaincode.prototype.reportAsRun = function (uid, inputArgs, cb) {
+    console.log(TAG, '- reportAsRun uid: ', uid);
+    console.log(TAG, '- reportAsRun input args: ', JSON.stringify(inputArgs));
+    var reportAsRun = {
         chaincodeID: this.chaincodeID,
-        fcn: 'createAccount',
-        args: [enrollID]
+        fcn: 'reportAsRun',
+        args: inputArgs
     };
 
-    invoke(this.chain, enrollID, createRequest, function (err, result) {
+    invoke(this.chain, uid, reportAsRun, function (err, result) {
         if (err) {
-            console.error(TAG, 'failed to create company:', err);
+            console.error(TAG, 'failed reportAsRun:', err);
             return cb(err);
         }
 
-        console.log(TAG, 'Created company successfully:', result.toString());
+        console.log(TAG, 'reportAsRun successfully:', result.toString());
         cb(null, result);
+    });
+}
+
+
+CPChaincode.prototype.queryTraceAdSpots = function (enrollID, inputArgs, cb) {
+    console.log(TAG, 'queryTraceAdSpots - chaincode_ops:', enrollID);
+
+    var queryTraceAdSpots = {
+        chaincodeID: this.chaincodeID,
+        fcn: 'queryTraceAdSpots',
+        args: inputArgs
+    };
+
+    query(this.chain, enrollID, queryTraceAdSpots, function (err, qResponse) {
+        if (err) {
+            console.error(TAG, 'failed to get queryTraceAdSpots:', err);
+            return cb(err);
+        }
+
+        console.log(TAG, 'retrieved queryTraceAdSpots information:', qResponse.toString());
+        cb(null, qResponse.toString());
     });
 };
 
@@ -133,30 +227,6 @@ CPChaincode.prototype.getBlockchainRecord = function (enrollID, recordKey, cb) {
         cb(null, papers.toString());
     });
 };
-
-
-CPChaincode.prototype.getCoverages = function (enrollID, subscriberID, cb) {
-    console.log(TAG, 'getting commercial papers');
-
-    // Accounts will be named after the enrolled users
-    var getPapersRequest = {
-        chaincodeID: this.chaincodeID,
-        fcn: 'getCoverages',
-        args: [subscriberID]
-    };
-
-    query(this.chain, enrollID, getPapersRequest, function (err, papers) {
-
-        if (err) {
-            console.error(TAG, 'failed to getPapers:', err);
-            return cb(err);
-        }
-
-        console.log(TAG, 'got papers');
-        cb(null, papers.toString());
-    });
-};
-
 
 /**
  * Helper function for invoking chaincode using the hfc SDK.
